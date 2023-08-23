98cf4eb02d592e65bc3d7463315137b3151afb28
GabrielOGW <gabrielogassawara2001@gmail.com>
2023-08-23 11:15:32 -0300

    filtrando retorno de erros no backend

--- Crud_v2/backend/app/Http/Requests/SavePessoaRequest.php
+++ Crud_v2/backend/app/Http/Requests/SavePessoaRequest.php
@@ -29,4 +29,29 @@ class SavePessoaRequest extends FormRequest
             'cargo' => ['required', 'string', 'max:255'],
         ];
     }
+
+    /**
+     * Get custom error messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'nome.required' => 'O campo nome é obrigatório.',
+            'nome.min' => 'O campo nome deve ter pelo menos 3 caracteres.',
+            'nome.max' => 'O campo nome não deve ter mais de 50 caracteres.',
+            'email.required' => 'O campo email é obrigatório.',
+            'email.email' => 'O campo email deve ser um endereço de e-mail válido.',
+            'email.max' => 'O campo email não deve ter mais de 255 caracteres.',
+            'data_nascimento.required' => 'O campo data de nascimento é obrigatório.',
+            'data_nascimento.date' => 'O campo data de nascimento deve ser uma data válida.',
+            'idade.required' => 'O campo idade é obrigatório.',
+            'idade.integer' => 'O campo idade deve ser um número inteiro.',
+            'idade.min' => 'O campo idade não deve ser negativo.',
+            'cargo.required' => 'O campo cargo é obrigatório.',
+            'cargo.string' => 'O campo cargo deve ser uma string.',
+            'cargo.max' => 'O campo cargo não deve ter mais de 255 caracteres.',
+        ];
+    }
 }
